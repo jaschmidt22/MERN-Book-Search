2e352a483876b7fe600584b587db818fa6f4d4db
jaschmidt22 <146290812+jaschmidt22@users.noreply.github.com>
2024-03-26 10:46:27 -0700

    added type defs and auth

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -1,18 +1,12 @@
-// typeDefs.js: Define the necessary Query and Mutation types:
-
-// Query type:
-
-//     me: Which returns a User type.
-
 // Mutation type:
 
 //     login: Accepts an email and password as parameters; returns an Auth type.
 
 //     addUser: Accepts a username, email, and password as parameters; returns an Auth type.
 
-//     aveBook: Accepts a book author's array, description, title, bookId, image, and link as parameters; returns a User type. (Look into creating what's known as an input type to handle all of these parameters!)
+//     saveBook: Accepts a book author's array, description, title, bookId, image, and link as parameters; returns a User type. (Look into creating what's known as an input type to handle all of these parameters!)
 
-//     emoveBook: Accepts a book's bookId as a parameter; returns a User type.
+//     removeBook: Accepts a book's bookId as a parameter; returns a User type.
 
 // -User type:
 
@@ -45,3 +39,62 @@
 //     token
 
 //     user (References the User type.)
+// typeDefs.js: Define the necessary Query and Mutation types:
+
+// Query type:
+
+//     me: Which returns a User type.
+const { gql } = require("apollo-server-express");
+
+const typeDefs = gql`
+  type Query {
+    me: User
+  }
+
+  type Mutation {
+    login(email: String!, password: String!): Auth
+    addUser(username: String!, email: String!, password: String!): Auth
+    saveBook(
+      authors: [String]
+      description: String!
+      title: String!
+      bookId: ID!
+      image: String
+      link: String
+    ): User
+    removeBook(bookId: ID!): User
+  }
+
+  type User {
+    _id: ID
+    username: String
+    email: String
+    bookCount: Int
+    savedBooks: [Book]
+  }
+
+  type Book {
+    bookId: ID
+    authors: [String]
+    description: String
+    title: String
+    image: String
+    link: String
+  }
+
+  type Auth {
+    token: ID!
+    user: User
+  }
+
+  input BookInput {
+    authors: [String]
+    description: String!
+    title: String!
+    bookId: ID!
+    image: String
+    link: String
+  }
+`;
+
+module.exports = typeDefs;
